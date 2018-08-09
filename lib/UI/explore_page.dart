c25c51dae1ddb73c6015569afceb1fcbb5bbeec3
Christopher Andrews <chrisandrews1133@gmail.com>
2018-08-09 13:16:34 +0100

    Architecture Changes

---
+++ lib/UI/explore_page.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:login/home_page.dart';
+
+class ExplorePage extends StatelessWidget {
+  static String tag = 'ExplorePage';
+
+  @override
+  Widget build(BuildContext context) {
+    final title = 'Items that you can recycle';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: new AppBar(
+          backgroundColor: Colors.green,
+          title: const Text("Evergreen", style: TextStyle(color: Colors.white)),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.home),
+              onPressed: () {
+                Navigator.of(context).pushReplacementNamed(HomePage.tag);
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.explore),
+            )
+          ],
+        ),
+        body: ListView(
+          children: <Widget>[
+            ListTile(
+              contentPadding: EdgeInsets.fromLTRB(20.0, 20.0, 0.0, 0.0),
+              title: Text(
+                "Recyclable Item List",
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontFamily: 'roboto',
+                    fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(
+                  "A list of the items you can and cant recycle in your area. "),
+            ),
+            ListTile(
+              contentPadding: EdgeInsets.only(left: 20.0),
+              title: Text(
+                'Recycling Labels',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontFamily: 'roboto',
+                    fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(
+                  "Definition of every recycling label printed on goods sold in the United Kingdom"),
+            ),
+            ListTile(
+              contentPadding: EdgeInsets.only(left: 20.0),
+              title: Text(
+                'Notifications',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontFamily: 'roboto',
+                    fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(
+                  "Get notified to changes to waste collection in your area"),
+            ),
+            ListTile(
+              contentPadding: EdgeInsets.only(left: 20.0),
+              title: Text(
+                'Recycling Group',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontFamily: 'roboto',
+                    fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(
+                  "Invite Members, form a group and view group recycling statistics"),
+            ),
+            ListTile(
+              contentPadding: EdgeInsets.only(left: 20.0),
+              title: Text(
+                'Application Settings',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontFamily: 'roboto',
+                    fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(
+                  "Change accessibility settings, legal information and much more"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
